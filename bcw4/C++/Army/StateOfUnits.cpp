663eefacbc7c48c45f5cb07741d09deb73539158
ehordyenko1 <91217030+ehordyenko1@users.noreply.github.com>
2021-12-24 19:28:53 +0200

    Delete bcw4/C++/Army directory

--- bcw4/C++/Army/StateOfUnits.cpp
+++
@@ -1,9 +0,0 @@
-#include "StateOfUnits.h"
-
-	
-std::ostream& operator<<(std::ostream& out, const Unit& unit) {
-	out << "HP of " << unit.getName() << " is " << unit.getHP() << "/" << unit.getHpLimit() << std::endl;
-	out << "Damage of " << unit.getName() << " : " << unit.getDamage() << std::endl;
-	out << unit.getName() << " has " << unit.getPower() << " Damage" << std::endl;
-	return out;
-}
